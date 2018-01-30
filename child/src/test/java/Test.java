b98fc2db9a830fa5bc9cfa39fb8a55fcc62df3b6
IrinaDaniel <ira-daniel@yandex.ru>
2018-01-31 00:04:45 +0300

    task2

---
+++ child/src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ira on 31.01.2018.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+
+    }
+}
--- src/test/java/com/company/AnimalTest.java
+++ src/test/java/com/company/AnimalTest.java
@@ -1,7 +1,18 @@
 package com.company;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
 /**
  * Created by Ira on 24.01.2018.
  */
 public class AnimalTest {
+
+    @Test
+    public void testName() {
+        assertTrue(true);
+    }
 }
