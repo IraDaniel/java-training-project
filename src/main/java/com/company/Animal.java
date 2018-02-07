8b7e26f712453788633388a4c5965b8e8722e6da
Irina_Daniel <Irina_Daniel@epam.com>
2018-02-07 12:18:03 +0300

    test for servlet

--- src/main/java/com/company/Animal.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * Created by Ira on 24.01.2018.
- */
-public class Animal {
-}
--- src/main/java/com/company/servlet/SimpleHttpServlet.java
+++ src/main/java/com/company/servlet/SimpleHttpServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SimpleHttpServlet extends HttpServlet {
 
@@ -17,13 +18,21 @@ public class SimpleHttpServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        print();
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        print();
+        response.setContentType( "text/html" );
+        PrintWriter out = response.getWriter();
+        out.println( "<html><head>" );
+        out.println( "<title>A Sample Servlet!</title>" );
+        out.println( "</head>" );
+        out.println( "<body>" );
+        out.println( "<h1>Hello, World!</h1>" );
+        out.println( "</body></html>" );
+        out.close();
     }
 
     private void print() {
